fef49f5d592fa916e2dfa2b7022245d1844676f1
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-07 15:38:16 +0000

    Item11808: Sorry, don't know what went wrong with my seletion, but few plugins has already been fixed by Micha, and got reverted by mistake.
    git-svn-id: http://svn.foswiki.org/trunk/JQSerialPagerContrib@14770 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- pub/System/JQSerialPagerContrib/jquery.serialpager.uncompressed.js
+++ pub/System/JQSerialPagerContrib/jquery.serialpager.uncompressed.js
@@ -121,7 +121,7 @@
   $(function() {
     $(".jqSerialPager:not(.jqInitedSerialPager)").livequery(function() {
       var $this = $(this),
-          opts = $.extend({}, defaults, $this.metadata());
+          opts = $.extend({}, defaults, $this.data(), $this.metadata());
 
       $this.addClass("jqInitedSerialPager").serialPager(opts);
     });
